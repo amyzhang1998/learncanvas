e9d79483f7a4f8682851f9800bfee4cc176855f5
amyzhang1998 <328110606@qq.com>
2017-07-31 23:23:58 +0800

    zx

--- Tools/util.js
+++ Tools/util.js
@@ -1,137 +1,166 @@
 //使用requestAnimationFrame的动画循环
 //polifile
 if (!window.requestAnimationFrame) {
-    widow.requestAnimationFrame = (
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.oRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function (callback) {
-            return window.setTimeout(callback, 1000 / 60)
-        })
+  widow.requestAnimationFrame =
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function(callback) {
+      return window.setTimeout(callback, 1000 / 60);
+    };
 }
 //工具函数
 var utils = {};
-utils.captureMouse = function (element) {
-    var mouse = {
-        x: 0,
-        y: 0
-    };
-    element.addEventListener('mousemove', function (event) {
-        var x, y;
-        if (event.pageX || event.pageY) {
-            x = event.pageX;
-            y = event.pageY;
-        } else {
-            x = event.clientX + document.body.scrollLeft +
-                document.documentElement.scrollLeft;
-            y = event.clientY + document.body.scrollTop +
-                document.documentElement.scrollTop;
-        }
-        x -= element.scrollLeft;
-        y -= element.scrollTop;
-        mouse.x = x;
-        mouse.y = y;
-
-    }, false);
-    return mouse;
-}
-utils.captureTouch = function (element) {
-    var touch = {
-        x: null,
-        y: null,
-        isPressed: false
-    };
-
-    element.addEventListener('touchstart', function (event) {
-        touch.isPressed = true;
-    }, false);
-
-    element.addEventListener('touchend', function (event) {
-        touch.isPressed = false;
-        touch.x = null;
-        touch.y = null;
-    }, false);
-
-    element.addEventListener('touchmove', function (event) {
-        var x, y,
-            touch_event = event.touches[0]; //first touch
-
-        if (touch_event.pageX || touch_event.pageY) {
-            x = touch_event.pageX;
-            y = touch_event.pageY;
-        } else {
-            x = touch_event.clientX + document.body.scrollLeft +
-                document.documentElement.scrollLeft;
-            y = touch_event.clientY + document.body.scrollTop +
-                document.documentElement.scrollTop;
-        }
-
-        x -= element.offsetLeft;
-        y -= element.offsetTop;
-
-        touch.x = x;
-        touch.y = y;
-    }, false);
-
-    return touch;
+utils.captureMouse = function(element) {
+  var mouse = {
+    x: 0,
+    y: 0
+  };
+  element.addEventListener(
+    "mousemove",
+    function(event) {
+      var x, y;
+      if (event.pageX || event.pageY) {
+        x = event.pageX;
+        y = event.pageY;
+      } else {
+        x =
+          event.clientX +
+          document.body.scrollLeft +
+          document.documentElement.scrollLeft;
+        y =
+          event.clientY +
+          document.body.scrollTop +
+          document.documentElement.scrollTop;
+      }
+      x -= element.scrollLeft;
+      y -= element.scrollTop;
+      mouse.x = x;
+      mouse.y = y;
+    },
+    false
+  );
+  return mouse;
 };
-utils.parseColor = function (color, toNumber) {
-    if (toNumber === true) {
-        if (typeof color === 'number') {
-            return (color | 0);
-        }
-        if (typeof color === 'string' && color[0] === '#') {
-            color = color.slice(1);
-        }
-        return window.parseColor(color, 16);
-    } else {
-        if (typeof color === 'number') {
-            color = '#' + ('00000' + (color | 0).toString()).substr(-6);
-        }
-        return color;
+utils.captureTouch = function(element) {
+  var touch = {
+    x: null,
+    y: null,
+    isPressed: false
+  };
+
+  element.addEventListener(
+    "touchstart",
+    function(event) {
+      touch.isPressed = true;
+    },
+    false
+  );
+
+  element.addEventListener(
+    "touchend",
+    function(event) {
+      touch.isPressed = false;
+      touch.x = null;
+      touch.y = null;
+    },
+    false
+  );
+
+  element.addEventListener(
+    "touchmove",
+    function(event) {
+      var x,
+        y,
+        touch_event = event.touches[0]; //first touch
+
+      if (touch_event.pageX || touch_event.pageY) {
+        x = touch_event.pageX;
+        y = touch_event.pageY;
+      } else {
+        x =
+          touch_event.clientX +
+          document.body.scrollLeft +
+          document.documentElement.scrollLeft;
+        y =
+          touch_event.clientY +
+          document.body.scrollTop +
+          document.documentElement.scrollTop;
+      }
+
+      x -= element.offsetLeft;
+      y -= element.offsetTop;
+
+      touch.x = x;
+      touch.y = y;
+    },
+    false
+  );
+
+  return touch;
+};
+utils.parseColor = function(color, toNumber) {
+  if (toNumber === true) {
+    if (typeof color === "number") {
+      return color | 0;
     }
-}
-
-utils.colorToRGB = function (color, alpha) {
-    //if string format , conver to number
-    if (typeof color === 'string' && color[0] === '#') {
-        color = window.parseInt(color.slice(1), 16);
+    if (typeof color === "string" && color[0] === "#") {
+      color = color.slice(1);
     }
-    alpha = (alpha === undefined) ? 1 : alpha;
+    return window.parseColor(color, 16);
+  } else {
+    if (typeof color === "number") {
+      color = "#" + ("00000" + (color | 0).toString()).substr(-6);
+    }
+    return color;
+  }
+};
 
-    //extract component values
-    var r = color >> 16 & 0xff,
-        g = color >> 8 & 0xff,
-        b = color & 0xff,
-        a = (alpha < 0) ? 0 : ((alpha > 1) ? 1 : alpha); //check range
+utils.colorToRGB = function(color, alpha) {
+  //if string format , conver to number
+  if (typeof color === "string" && color[0] === "#") {
+    color = window.parseInt(color.slice(1), 16);
+  }
+  alpha = alpha === undefined ? 1 : alpha;
 
-    // use 'rgba' if needed
-    if (a === 1) {
-        return "rgba(" + r + "," + g + "," + b + ")";
-    } else {
-        return "rgba(" + r + "," + g + "," + b + "," + a + ")";
+  //extract component values
+  var r = (color >> 16) & 0xff,
+    g = (color >> 8) & 0xff,
+    b = color & 0xff,
+    a = alpha < 0 ? 0 : alpha > 1 ? 1 : alpha; //check range
 
-    };
-}
+  // use 'rgba' if needed
+  if (a === 1) {
+    return "rgba(" + r + "," + g + "," + b + ")";
+  } else {
+    return "rgba(" + r + "," + g + "," + b + "," + a + ")";
+  }
+};
 //决定一个指定的坐标位置是否位于矩阵的边界内
-utils.containsPoint = function (rect, x, y) {
-    console.log(rect);
-    return !(
-        x < rect.x || x > rect.x + rect.width ||
-        y < rect.y || y > rect.y + rect.height
-    );
-}
-utils.containsCanvas = function (canvas, x, y) {
-    return !(
-        x < rect.x || x > rect.x + rect.width ||
-        y < rect.y || y > rect.y + rect.height
-    );
-}
+utils.containsPoint = function(rect, x, y) {
+  console.log(rect, x, y, y < rect.y, y > rect.y + rect.height);
+  return !(
+    x < rect.x ||
+    x > rect.x + rect.width ||
+    y < rect.y ||
+    y > rect.y + rect.height
+  );
+};
+utils.containsCanvas = function(canvas, x, y) {
+  return !(
+    x < rect.x ||
+    x > rect.x + rect.width ||
+    y < rect.y ||
+    y > rect.y + rect.height
+  );
+};
 //如果两个矩形对象相交，就返回true,否则返回false
-utils.intersects = function (rectA, rectB) {
-    return !(rectA.x + rectA.width < rectB.x ||
-        rectB.x + rectB.width < rectA.x ||
-        rectA.y + rectA.height < rectB.y ||
-        rectB.y + rectB.height < rectA.y);
-}
+utils.intersects = function(rectA, rectB) {
+  return !(
+    rectA.x + rectA.width < rectB.x ||
+    rectB.x + rectB.width < rectA.x ||
+    rectA.y + rectA.height < rectB.y ||
+    rectB.y + rectB.height < rectA.y
+  );
+};
